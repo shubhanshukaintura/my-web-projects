93e005834bceb77c244af8500c62f8d2fb20f062
Shubhanshu Kaintura <shubhanshukaintura2@gmail.com>
2024-06-01 20:49:10 +0530

    Added a highlighter on today date

--- React/productiveApp/src/components/Calender.jsx
+++ React/productiveApp/src/components/Calender.jsx
@@ -31,14 +31,19 @@ function Calendar() {
       adjustedYear = currentYear + Math.floor(currentMonth / 12);
     }
 
-    setMonth(monthName[adjustedMonth]+' '+adjustedYear);
+    setMonth(monthName[adjustedMonth] + ' ' + adjustedYear);
 
     const firstDay = new Date(adjustedYear, adjustedMonth, 1).getDay();
     const lastDay = new Date(adjustedYear, adjustedMonth + 1, 0).getDate();
     const daysArr = Array.from({ length: firstDay > 0 ? firstDay - 1 : 6 }).fill(0);
+    const today = new Date().getDate();
+    const isCurrentMonth = date.getMonth() === adjustedMonth && date.getFullYear() === adjustedYear;
 
     for (let i = 1; i <= lastDay; i++) {
-      daysArr.push(i);
+      daysArr.push({
+        day: i,
+        isToday: isCurrentMonth && i === today
+      });
     }
 
     setDays(daysArr);
@@ -63,7 +68,11 @@ function Calendar() {
         </ul>
         <ul className="flex flex-wrap items-center gap-1 py-2 px-3">
           {days.map((day, index) => (
-            <li key={index} className="w-[50px] h-[50px] flex justify-center items-center border">{day !== 0 ? day : ''}</li>
+            <li 
+              key={index} 
+              className={`w-[50px] h-[50px] flex justify-center items-center border ${day.isToday ? 'bg-red-700 text-white' : ''}`}>
+              {day.day !== 0 ? day.day : ''}
+            </li>
           ))}
         </ul>
       </div>
